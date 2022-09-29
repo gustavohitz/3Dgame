61ec599b6678574f81e53efc94c65dfa0578f772
Gustavo Hitzschky <gust.campos@gmail.com>
2022-09-28 21:13:42 -0300

    Adjusting StateMachine and StateBase scripts

---
+++ Assets/Scripts/Ebac/StateBase.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateBase {
+
+    public virtual void OnStateEnter(object o = null) {
+
+    }
+    public virtual void OnStateStay() {
+
+    }
+    public virtual void OnStateExit() {
+
+    }
+}
