ab09560eaab48cda2de8b514b6e9a47986c999d9
Gustavo Hitzschky <gust.campos@gmail.com>
2022-10-17 17:25:14 -0300

    adding jump and running faster

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -8,6 +8,11 @@ public class Player : MonoBehaviour {
     public float speed = 1f;
     public float rotationSpeed = 1f;
     public float gravity = -9.8f;
+    public float jumpSpeed = 15f;
+    
+    [Header("Run Setup")]
+    public KeyCode runKey = KeyCode.LeftShift;
+    public float runningSpeed = 1.5f;
 
     private float _vSpeed = 0f;
 
@@ -17,17 +22,31 @@ public class Player : MonoBehaviour {
         var inputVerticalAxis = Input.GetAxis("Vertical");
         var speedVector = transform.forward * inputVerticalAxis * speed;
 
+        if(characterController.isGrounded) {
+            _vSpeed = 0;
+           if(Input.GetKeyDown(KeyCode.Space)) {
+                _vSpeed = jumpSpeed;
+            }
+        }
+
+        var isWalking = inputVerticalAxis != 0;
+        if(isWalking) {
+            if(Input.GetKey(runKey)) {
+                speedVector *= runningSpeed;
+                animator.speed = runningSpeed;
+            }
+            else {
+                animator.speed = 1;
+            }
+        }
+
+
         _vSpeed -= gravity * Time.deltaTime;
         speedVector.y = _vSpeed;
 
         characterController.Move(speedVector * Time.deltaTime);
 
-        if(inputVerticalAxis != 0) {
-            animator.SetBool("Run", true);
-        }
-        else {
-            animator.SetBool("Run", false);
-        }
+        animator.SetBool("Run", isWalking);
     }
   
 }
