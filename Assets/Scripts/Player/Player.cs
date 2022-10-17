f1d42c1fa4e038e7ed54ec0f542dba1e5fa2f826
Gustavo Hitzschky <gust.campos@gmail.com>
2022-10-17 16:47:10 -0300

    Adding running and idle animation

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    public Animator animator;
     public CharacterController characterController;
     public float speed = 1f;
     public float rotationSpeed = 1f;
@@ -12,13 +13,21 @@ public class Player : MonoBehaviour {
 
     void Update() {
         transform.Rotate(0, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime, 0);
-        
-        var speedVector = transform.forward * Input.GetAxis("Vertical") * speed;
+
+        var inputVerticalAxis = Input.GetAxis("Vertical");
+        var speedVector = transform.forward * inputVerticalAxis * speed;
 
         _vSpeed -= gravity * Time.deltaTime;
         speedVector.y = _vSpeed;
 
         characterController.Move(speedVector * Time.deltaTime);
+
+        if(inputVerticalAxis != 0) {
+            animator.SetBool("Run", true);
+        }
+        else {
+            animator.SetBool("Run", false);
+        }
     }
   
 }
