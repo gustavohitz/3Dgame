2a63158b3f662e15d784e347d7f892e90df85e56
Gustavo Hitzschky <gust.campos@gmail.com>
2022-10-28 19:36:39 -0300

    Adding triggers to Cinemachine

---
+++ Assets/Scripts/Boss/BossStartCheck.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossStartCheck : MonoBehaviour {
+
+    public string tagToCheck = "Player";
+    public GameObject bossCamera;
+    public Color gizmoColor = Color.yellow;
+
+    void Awake() {
+        bossCamera.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.transform.tag == tagToCheck) {
+            TurnCameraOn();
+        }
+    }
+
+    void TurnCameraOn() {
+        bossCamera.SetActive(true);
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawSphere(transform.position, transform.localScale.y);
+    }
+    
+}
