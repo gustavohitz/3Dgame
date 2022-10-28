8ab3a5f6463aa59afe7052faa4d9902c198f530c
Gustavo Hitzschky <gust.campos@gmail.com>
2022-10-28 17:11:34 -0300

    Adding postprocessing in real time

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -88,6 +88,7 @@ public class Player : MonoBehaviour {//, IDamageable
     #region LIFE
     public void Damage(HealthBase h) {
         flashColors.ForEach(i => i.Flash());
+        EffectsManager.Instance.ChangeVignette();
     }
     public void Damage(float damage, Vector3 dir) {
         //Damage(damage);
