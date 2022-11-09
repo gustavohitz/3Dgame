e25614f85be1cd7197ac77ca88858ce299d01179
Gustavo Hitzschky <gust.campos@gmail.com>
2022-11-09 18:18:12 -0300

    Adding more items to collect

--- Assets/Scripts/Items/ItemCollectableBase.cs
+++ Assets/Scripts/Items/ItemCollectableBase.cs
@@ -1,43 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Items;
 
-public class ItemCollectableBase : MonoBehaviour {
+namespace Items {
+    public class ItemCollectableBase : MonoBehaviour {
+        public ItemType itemType;
 
-    public string compareTag = "Player";
-    public ItemManager itemManager;
-    //public HUDBase hudBase;
-    public ParticleSystem particleSystem;
+        public string compareTag = "Player";
+        public ItemManager itemManager;
+        //public HUDBase hudBase;
+        public ParticleSystem particleSystem;
 
-    [Header("Sounds")]
-    public AudioSource audioSource;
+        [Header("Sounds")]
+        public AudioSource audioSource;
 
-    void Awake() {
-        if(particleSystem != null) {
-            particleSystem.transform.SetParent(null);
-        }
-    }
-
-    void OnTriggerEnter(Collider other) {
-        if(other.transform.CompareTag(compareTag)) {
-            Collect();
-            //hudBase.UIUpdate();
-        }
-    }
-    protected virtual void Collect() {
-        Destroy(gameObject);
-        OnCollect();
-    }
-
-    protected virtual void OnCollect() {
-        if(particleSystem != null) {
-            particleSystem.Play();
+        void Awake() {
+            if(particleSystem != null) {
+                particleSystem.transform.SetParent(null);
+            }
         }
 
-        if(audioSource != null) {
-            audioSource.Play();
+        void OnTriggerEnter(Collider other) {
+            if(other.transform.CompareTag(compareTag)) {
+                Collect();
+                //hudBase.UIUpdate();
+            }
         }
+        protected virtual void Collect() {
+            Destroy(gameObject);
+            OnCollect();
+        }
+
+        protected virtual void OnCollect() {
+            if(particleSystem != null) {
+                particleSystem.Play();
+            }
+
+            if(audioSource != null) {
+                audioSource.Play();
+            }
+            
+            ItemManager.Instance.AddByType(itemType);
+        }
+        
     }
-    
 }
