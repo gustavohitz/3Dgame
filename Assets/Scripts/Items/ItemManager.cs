1c523648eaeecc3785415ba8356d6dd6d939f80e
Gustavo Hitzschky <gust.campos@gmail.com>
2022-11-09 17:51:57 -0300

    Adding inventory

--- Assets/Scripts/Items/ItemManager.cs
+++ Assets/Scripts/Items/ItemManager.cs
@@ -3,31 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using Ebac.Core.Singleton;
 
-public class ItemManager : Singleton<ItemManager> {
+namespace Items {
+    
 
-    public SOInt coins;
-    public SOInt satellites;
-    public SOInt planets;
-
-    void Start() {
-        Reset();
+    public enum ItemType {
+        COIN,
+        LIFE_PACK
     }
 
-    private void Reset() {
-        coins.value = 0;
-        satellites.value = 0;
-        planets.value = 0;
+    public class ItemManager : Singleton<ItemManager> {
+        public List<ItemSetup> itemSetups;
+
+
+        void Start() {
+            Reset();
+        }
+
+        private void Reset() {
+            foreach(var i in itemSetups) {
+                i.soInt.value = 0;
+            }
+        }
+
+        public void AddByType(ItemType itemType, int amount = 1) {
+            if(amount < 0) return;
+            itemSetups.Find(i => i.itemType == itemType).soInt.value += amount;
+        }
+        public void RemoveByType(ItemType itemType, int amount = -1) {
+            if(amount > 0) return;
+
+            var item = itemSetups.Find(i => i.itemType == itemType);
+            item.soInt.value -= amount;
+
+            if(item.soInt.value < 0) {
+                item.soInt.value = 0;
+            }
+        }
+
+        [NaughtyAttributes.Button]
+        private void AddCoin() {
+            AddByType(ItemType.COIN);
+        }
+        
+        [NaughtyAttributes.Button]
+        private void AddLifePack() {
+            AddByType(ItemType.LIFE_PACK);
+        }
+    
     }
 
-    public void AddCoins(int amount = 1) {
-        coins.value += amount;
-    }
 
-    public void AddSatellites(int amount = 1) {
-        satellites.value += amount;
+    [System.Serializable]
+    public class ItemSetup {
+        public ItemType itemType;
+        public SOInt soInt;
     }
-    public void AddPlanets(int amount = 1) {
-        planets.value += amount;
-    }
-   
 }
