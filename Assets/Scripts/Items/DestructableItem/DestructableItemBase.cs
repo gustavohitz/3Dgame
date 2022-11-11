5557745ea724003ab0a74a35a77ca634702d24b4
Gustavo Hitzschky <gust.campos@gmail.com>
2022-11-11 20:52:19 -0300

    Adding coins that drop from trees

--- Assets/Scripts/Items/DestructableItem/DestructableItemBase.cs
+++ Assets/Scripts/Items/DestructableItem/DestructableItemBase.cs
@@ -7,6 +7,9 @@ public class DestructableItemBase : MonoBehaviour {
     public HealthBase healthBase;
     public float shakeDuration = .1f;
     public int shakeVibrato = 5;
+    public int dropCoinsAmount = 10;
+    public GameObject coinPrefab;
+    public Transform dropPosition;
 
     void OnValidate() {
         if(healthBase == null) {
@@ -19,5 +22,24 @@ public class DestructableItemBase : MonoBehaviour {
     }
     void OnDamage(HealthBase h) {
         transform.DOShakeScale(shakeDuration, Vector3.up/2, shakeVibrato);
+        DropGroupOfCoins();
+    }
+
+    [NaughtyAttributes.Button]
+    void DropCoins() {
+        var i = Instantiate(coinPrefab);
+        i.transform.position = dropPosition.position;
+        i.transform.DOScale(0, 1f).SetEase(Ease.OutBack).From();
+    }
+
+    [NaughtyAttributes.Button]
+    void DropGroupOfCoins() {
+        StartCoroutine(DropGroupOfCoinsCoroutine());
+    }
+    IEnumerator DropGroupOfCoinsCoroutine() {
+        for(int i = 0; i < dropCoinsAmount; i++) {
+            DropCoins();
+            yield return new WaitForSeconds(.1f);
+        }
     }
 }
