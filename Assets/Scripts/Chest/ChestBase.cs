ebbe66e46a64dcbbc7970ec98a43fbaa8a6fed7a
Gustavo Hitzschky <gust.campos@gmail.com>
2022-11-11 10:22:45 -0300

    Adding chest

---
+++ Assets/Scripts/Chest/ChestBase.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ChestBase : MonoBehaviour {
+    public Animator animator;
+    public string triggerOpen = "Open";
+
+    [Header("Notification")]
+    public GameObject notification;
+    public float tweenDuration = .2f;
+    public Ease tweenEase = Ease.OutBack;
+    private float _startScale;
+
+    void Start() {
+        _startScale = notification.transform.localScale.x;
+        HideNotification();
+    }
+
+    [NaughtyAttributes.Button]
+    private void OpenChest() {
+        animator.SetTrigger(triggerOpen);
+    }
+    void OnTriggerEnter(Collider other) {
+        Player p = other.transform.GetComponent<Player>();
+        if(p != null) {
+            ShowNotification();
+        }
+    }
+    void OnTriggerExit(Collider other) {
+        Player p = other.transform.GetComponent<Player>();
+        if(p != null) {
+            HideNotification();
+        }
+    }
+    
+    [NaughtyAttributes.Button]
+    void ShowNotification() {
+        notification.SetActive(true);
+        notification.transform.localScale = Vector3.zero;
+        notification.transform.DOScale(_startScale, tweenDuration);
+    }
+    
+    [NaughtyAttributes.Button]
+    void HideNotification() {
+        notification.SetActive(false);
+    }
+    
+}
