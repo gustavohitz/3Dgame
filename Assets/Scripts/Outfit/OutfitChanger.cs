f05f358398816f8463790474d0de43a3dc7f59d0
Gustavo Hitzschky <gust.campos@gmail.com>
2022-11-16 12:12:57 -0300

    Adding new outfits to Player

---
+++ Assets/Scripts/Outfit/OutfitChanger.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutfitChanger : MonoBehaviour {
+   public SkinnedMeshRenderer mesh;
+   public Texture2D texture;
+   public string shaderIdName = "_EmissionMap";
+
+   [NaughtyAttributes.Button]
+   private void ChangeTexture() {
+    mesh.materials[0].SetTexture(shaderIdName, texture);
+   }
+}
